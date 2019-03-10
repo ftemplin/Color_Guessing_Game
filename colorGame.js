5f72d627cbaa9a55e50abe0db510048693dbbdba
ftemplin <Fritz.templin@gmail.com>
2019-03-10 16:25:45 -0700

    colorGame.js: corrected variable naming for color assignment to squares.

--- colorGame.js
+++ colorGame.js
@@ -8,15 +8,15 @@ var colors = [
 	"rgb(0, 200, 200)",
 ]
 
-<<<<<<< HEAD
+
 var squares = document.querySelectorAll(".square");
 
 for(var i = 0; i < squares.length; i++){
-=======
+
 var squares = document.querySelectorAll(".squares")
 
 for(var i = 0; i < square.length; i++){
->>>>>>> 99838bc0df3795180731f5e2a46b74d42b9b745a
+
 	squares[i].style.backgroundColor = colors[i];
 }
 
