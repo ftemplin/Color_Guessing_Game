602c93b83f44fc3f3aa0fdb80d8068594ec0ee33
ftemplin <Fritz.templin@gmail.com>
2019-03-10 16:39:08 -0700

    colorGame.js: corrections due to last commit merger errors, added static color selector for guessed color.

--- colorGame.js
+++ colorGame.js
@@ -5,18 +5,16 @@ var colors = [
 	"rgb(0, 255, 0)",
 	"rgb(200, 200, 0)",
 	"rgb(200, 0, 200)",
-	"rgb(0, 200, 200)",
+	"rgb(0, 200, 200)"
 ]
-
-
 var squares = document.querySelectorAll(".square");
+var colorPicked = colors[3];
+var colorDisplay = document.querySelector("#colorDisplay");
+
+colorDisplay.textContent = colorPicked;
+
 
 for(var i = 0; i < squares.length; i++){
-
-var squares = document.querySelectorAll(".squares")
-
-for(var i = 0; i < square.length; i++){
-
 	squares[i].style.backgroundColor = colors[i];
 }
 
